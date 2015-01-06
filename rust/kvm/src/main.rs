2c90814afc80b3ae778042dddb8240893c18b0de
anurse <andrew@andrewnurse.net>
2015-01-05 16:55:10 -0800

    Started on command line options stuff

--- rust/kvm/src/main.rs
+++ rust/kvm/src/main.rs
@@ -1,3 +1,16 @@
+#![feature(phase)]
+
+extern crate docopt;
+
+use docopt::Docopt;
+
+static USAGE: &'static str = "
+Usage: kvm help [options]
+       kvm get-latest [options]
+
+Options:
+	-f <feed-url>, --feed <feed-url>
+";
+
 fn main() {
-    println!("Hello, world!")
 }
